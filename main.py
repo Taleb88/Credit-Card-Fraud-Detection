2b8a6de060b0c86214e3ddc96b20bec45f79b0da
Taleb88 <tdamaree18@gmail.com>
2024-11-26 22:29:39 -0500

    created csv file containing top 20 amount

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 df = pd.read_csv('creditcard.csv')
 
-print(df)
+#print(df)
+
+print(df.head())
+print(df.tail())
+
+sorted_column = df.sort_values(['Amount'], ascending=False)
+top_20_amount = sorted_column.head(20)
+
+top_20_amount.to_csv('top_20_amount.csv', index=False)
