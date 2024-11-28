b0f0e38b86e1c5988e44747a67b87edddae724bc
Taleb88 <tdamaree18@gmail.com>
2024-11-27 23:28:55 -0500

    updated 'Entry' column in top_20_amount.csv to 'Case'.

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ df = pd.read_csv('creditcard.csv')
 sorted_column = df.sort_values(['Amount'], ascending=False)
 top_20_amount = sorted_column.head(20)
 
-top_20_amount = top_20_amount.rename({'Time': 'Entry'}, axis='columns')
+top_20_amount = top_20_amount.rename({'Time': 'Case'}, axis='columns')
 
 top_20_amount.to_csv('top_20_amount.csv', index=False)
 
-# creating a pie chart based on top 20 amount
-#x_axis = top_20_amount['Amount']
-#y_axis = top_20_amount['Amount']
+# creating a bar chart via matplotlib
+x_axis = top_20_amount['Case']
+y_axis = top_20_amount['Amount']
+
+plt.bar(x_axis, y_axis)
+
+plt.title('Amounts per Case')
+
+plt.xlabel('Entry')
+plt.ylabel('Amount')
+
+plt.show()
